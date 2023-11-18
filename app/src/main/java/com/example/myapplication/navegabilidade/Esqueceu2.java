bde8d2b36e0a84e2419f0fc96af54dbc48d40e12
Leonardo Reis <71906441+leonardoreiss@users.noreply.github.com>
2023-11-18 16:42:58 -0300

    Toda a parte de Esqueceu a Senha completo

---
+++ app/src/main/java/com/example/myapplication/navegabilidade/Esqueceu2.java
@@ -0,0 +1,36 @@
+package com.example.myapplication.navegabilidade;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+
+public class Esqueceu2 extends AppCompatActivity {
+    private Button btn_seguinte;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_esqueceu_senha2);
+
+        IniciarComponentes();
+
+        btn_seguinte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Esqueceu2.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void IniciarComponentes() {
+        btn_seguinte = findViewById(R.id.btn_seguinte);
+    }
+}
